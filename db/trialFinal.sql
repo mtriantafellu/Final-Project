8091cbffefeda93c68e391457ffcbd2eb221cc3f
Matthew <mtriantafellu@users.noreply.github.com>
2017-07-31 13:13:12 -0400

    with folders created

---
+++ db/trialFinal.sql
@@ -0,0 +1,34 @@
+#customerinfoCREATE DATABASE `trialFinal`;
+
+USE `trialFinal`;
+
+/*CREATE TABLE `userInfo` (
+id INT AUTO_INCREMENT,
+userName VARCHAR(255),
+googleEmail VARCHAR(255),
+phone INT,
+PRIMARY KEY (`id`)
+);*/
+
+/*CREATE TABLE `jobInfo` (
+id INT AUTO_INCREMENT,
+dateOfAppointment VARCHAR(255),
+repairTitle VARCHAR(255),
+repairQuote VARCHAR(255),
+actualCost VARCHAR(255),
+discounted BOOLEAN DEFAULT FALSE,
+complete BOOLEAN DEFAULT FALSE,
+completionDate VARCHAR(255),
+PRIMARY KEY (`id`)
+);*/
+
+/*CREATE TABLE `customerInfo` (
+id INT AUTO_INCREMENT,
+name VARCHAR(255),
+address VARCHAR(255),
+phone INT,
+email VARCHAR(255),
+previousCustomer BOOLEAN DEFAULT FALSE,
+goodCustomer VARCHAR(255),
+PRIMARY KEY (`id`)
+);*/
